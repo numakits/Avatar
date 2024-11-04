ff7a035205bdccd792aa6148f7556a6933412d14
numakits <sondoangov@gmail.com>
2024-11-04 11:01:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const photoContainer = document.getElementById("photo-container");
   
     function loadImages() {
-      fetch("/api/images")
+      fetch("/images") // Changed to fetch from background directory
         .then((response) => response.json())
         .then((images) => {
           photoContainer.innerHTML = "";
           images.forEach((src) => {
             const img = document.createElement("img");
-            img.src = src;
+            img.src = `/images/${src}`; // Update image source path
             img.alt = "Image Frame";
             img.classList.add("photo");
             img.addEventListener("click", () => handleSelectedPhoto(img));
@@ -26,4 +26,3 @@ document.addEventListener("DOMContentLoaded", () => {
     // Load images when page loads
     loadImages();
   });
-  
